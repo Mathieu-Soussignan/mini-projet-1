482462a049765ea3eecf297f60f99cf8d2e67c0b
Mathieu Soussignan <contact.mathieu.soussignan@gmail.com>
2025-02-19 16:52:13 +0100

    Update bugs linter

--- modules/database.py
+++ modules/database.py
@@ -1,11 +1,11 @@
 from models.base import Base, engine, SessionLocal
 
 # Importer tous les modèles pour qu'ils soient enregistrés dans le metadata
-from models.sex_model import Sex
-from models.region_model import Region
-from models.smoker_model import Smoker
-from models.app_user_model import AppUser
-from models.patient_model import Patient
+from models.sex_model import Sex            # noqa: F401
+from models.region_model import Region      # noqa: F401
+from models.smoker_model import Smoker      # noqa: F401
+from models.app_user_model import AppUser   # noqa: F401
+from models.patient_model import Patient    # noqa: F401
 
 def init_db():
     """Crée les tables dans la base de données."""
